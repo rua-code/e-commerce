0d48345797b1feda2998dda213ee1b9d19b34dfa
ro'a <ruaa.shamali@gmail.com>
2026-02-10 15:21:05 +0200

    fix router imports paths

--- src/router.jsx
+++ src/router.jsx
@@ -1,34 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Cart from "./pages/cart/Cart";
-import Login from "./pages/auth/login/login";
-import Register from "./pages/auth/register/register";
+import Login from "./pages/auth/login/Login";
 import Home from "./pages/home/home";
+import Register from "./pages/auth/register/register";
 
 
-const router=createBrowserRouter([
-    {
-        path:'/',
-        element:<MainLayout/>,
-        children:[{
-            index:true,
-            element:<Home/>
-
-        },
-        {
-            path:'cart',
-            element:<Cart/>
-        },
-        {
-            path:'login',
-            element:<Login/>
-        },
-        {
-            path:'register',
-            element:<Register/>
-        }
-    ]
-    }
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home/>,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
 export default router;
