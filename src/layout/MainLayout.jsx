795ffff118251a8ee8c9181562fa6b9d490d4670
ro'a <ruaa.shamali@gmail.com>
2026-02-10 15:30:42 +0200

    fix router imports paths

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../componentes/navbar/navbar'
-import Footer from '../componentes/footer/footer'
+import Navbar from '../componentes/navbar/Navbar.jsx'
+import Footer from '../componentes/footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
