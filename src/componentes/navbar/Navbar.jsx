0fe48ada1e0db3cdf2f0d27dd489ecf3a9447624
ro'a <ruaa.shamali@gmail.com>
2026-02-13 17:13:33 +0200

    navbar

--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
+  
+ 
   return (
-    <div>Navbar</div>
-  )
+   
+    <Box >
+      <AppBar position="static"
+      sx={{ backgroundColor: "white" }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            aria-label="menu"
+            sx={{ mr:2 ,display:{xs:'flex',sm:'none'} } }
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 ,color:"black"}}>
+           SWOO 
+           <br/>
+           TECH MART
+          </Typography>
+          <Box sx={{ display:{xs:'none',sm:'flex'}}}>
+ <Button  sx={{ color:"black"}}>Home</Button>
+           <Button sx={{ color:"black"}}>Pages</Button>
+            <Button sx={{ color:"black"}}>products</Button>
+             <Button sx={{ color:"black"}}>contact</Button>
+          </Box>
+         
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
+
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from '../componentes/navbar/Navbar.jsx'
+import Navbar from'../componentes/navbar/Navbar.jsx'
 import Footer from '../componentes/footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
   return (
    <>
-<Navbar />
+<Navbar/>
 <Outlet />
 <Footer />
    </>
